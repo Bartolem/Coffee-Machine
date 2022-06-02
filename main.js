2ef203a3d72fc79d4741f222daaaa464b79316cc
Bartolem <97914472+Bartolem@users.noreply.github.com>
2022-06-02 11:48:17 +0200

    fix functions

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 const input = require('sync-input')
 
-let waterQuantity;
-let milkQuantity;
-let coffeeBeansQuantity;
-let cupsQuantity;
-let money;
-let buy;
-let fill;
-let take;
+let buy = "buy";
+let fill = "fill";
+let take = "take";
 
 const coffeeMachine = {
     money: 550,
@@ -42,52 +37,80 @@ const cappuccino = {
 }
 
 function buyEspresso() {
-    const result = Object.keys(coffeeMachine).reduce((firstValue, secondValue) =>
-        (firstValue[secondValue] = coffeeMachine[secondValue] - espresso[secondValue], firstValue),{});
+    coffeeMachine.water = coffeeMachine.water - espresso.water;
+    coffeeMachine.milk = coffeeMachine.milk - espresso.milk;
+    coffeeMachine.beans = coffeeMachine.beans - espresso.beans;
+    coffeeMachine.cup = coffeeMachine.cup - espresso.cup;
+    coffeeMachine.money = coffeeMachine.money + espresso.money;
+}
 
-    return start();
+function buyLatte() {
+    coffeeMachine.water = coffeeMachine.water - latte.water;
+    coffeeMachine.milk = coffeeMachine.milk - latte.milk;
+    coffeeMachine.beans = coffeeMachine.beans - latte.beans;
+    coffeeMachine.cup = coffeeMachine.cup - latte.cup;
+    coffeeMachine.money = coffeeMachine.money + latte.money;
+}
+
+function buyCappuccino() {
+    coffeeMachine.water = coffeeMachine.water - cappuccino.water;
+    coffeeMachine.milk = coffeeMachine.milk - cappuccino.milk;
+    coffeeMachine.beans = coffeeMachine.beans - cappuccino.beans;
+    coffeeMachine.cup = coffeeMachine.cup - cappuccino.cup;
+    coffeeMachine.money = coffeeMachine.money + cappuccino.money;
+}
+
+function getStateOfCoffeeMachine() {
+    console.log("The coffee machine has: ");
+    console.log(`${coffeeMachine.water} ml of water`);
+    console.log(`${coffeeMachine.milk} ml of milk`);
+    console.log(`${coffeeMachine.beans} g of coffee beans`);
+    console.log(`${coffeeMachine.cup} disposable cups`);
+    console.log(`${coffeeMachine.money} of money\n`);
 }
 
 function start() {
-    console.log("The coffee machine has: ");
-    console.log(`${waterQuantity} ml of water`);
-    console.log(`${milkQuantity} ml of milk`);
-    console.log(`${coffeeBeansQuantity} g of coffee beans`);
-    console.log(`${cupsQuantity} disposable cups`);
-    console.log(`${money} of money\n`);
+    getStateOfCoffeeMachine();
+
+    let action = input("Write action (buy, fill, take):\n");
+
+    switch (action) {
+        case buy:
+            buy = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n");
+            switch (buy) {
+                case "1":
+                    buyEspresso();
+                    getStateOfCoffeeMachine();
+                    break;
+                case "2":
+                    buyLatte();
+                    getStateOfCoffeeMachine();
+                    break;
+                case "3":
+                    buyCappuccino();
+                    getStateOfCoffeeMachine();
+                    break;
+            }
+            break;
+        case fill:
+            coffeeMachine.water = coffeeMachine.water +
+                Number(input("Write how many ml of water you want to add:\n"));
+            coffeeMachine.milk = coffeeMachine.milk +
+                Number(input("Write how many ml of milk you want to add:\n"));
+            coffeeMachine.beans = coffeeMachine.beans +
+                Number(input("Write how many grams of coffee beans you want to add:\n"));
+            coffeeMachine.cup = coffeeMachine.cup +
+                Number(input("Write how many coffee cups you want to add:\n"));
+
+            getStateOfCoffeeMachine();
+            break;
+        case take:
+            console.log(`I gave you ${coffeeMachine.money}$\n`);
+            coffeeMachine.money = 0;
+
+            getStateOfCoffeeMachine();
+            break;
+    }
 }
 
 start();
-
-let action = input("Write action (buy, fill, take)\n");
-
-switch (action) {
-    case "buy":
-        buy = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n");
-        switch (buy) {
-            case "1":
-                console.log(buyEspresso());
-                break;
-            case "2":
-
-                break;
-            case "3":
-
-                break;
-        }
-
-        break;
-    case "fill":
-        waterQuantity = Number(input("Write how many ml of water you want to add:\n"));
-        milkQuantity = Number(input("Write how many ml of milk you want to add:\n"));
-        coffeeBeansQuantity = Number(input("Write how many grams of coffee beans you want to add:\n"));
-        cupsQuantity = Number(input("Write how many coffee cups you want to add:\n"));
-        break;
-    case "take":
-        console.log("I gave you $550");
-        break;
-}
-
-
-
-

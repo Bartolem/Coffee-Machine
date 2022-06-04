948b2fef3c475f9368a5523584aeb79acf6ef77e
Bartolem <97914472+Bartolem@users.noreply.github.com>
2022-06-04 13:43:02 +0200

    add more functions and while loop

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const input = require('sync-input')
 let buy = "buy";
 let fill = "fill";
 let take = "take";
+let remaining = "remaining";
+let exit = "exit";
+let terminateProgram = false;
+let terminateAction = false;
 
 const coffeeMachine = {
     money: 550,
@@ -36,6 +40,79 @@ const cappuccino = {
     cup: 1
 }
 
+function checkResourcesForEspresso() {
+    if (coffeeMachine.water < espresso.water) {
+        terminateAction = true;
+        console.log("Sorry, not enough water!\n");
+    }
+    else if (coffeeMachine.milk < espresso.milk) {
+        terminateAction = true;
+        console.log("Sorry, not enough milk!\n");
+    }
+    else if (coffeeMachine.beans < espresso.beans) {
+        terminateAction = true;
+        console.log("Sorry, not enough coffee beans!\n");
+    }
+    else if (coffeeMachine.cup < espresso.cup) {
+        terminateAction =true;
+        console.log("Sorry, not enough cups!\n");
+    }
+    else {
+        buyEspresso();
+        enoughResourcesMessage();
+    }
+}
+
+function checkResourcesForLatte() {
+    if (coffeeMachine.water < latte.water) {
+        terminateAction = true;
+        console.log("Sorry, not enough water!\n");
+    }
+    else if (coffeeMachine.milk < latte.milk) {
+        terminateAction = true;
+        console.log("Sorry, not enough milk!\n");
+    }
+    else if (coffeeMachine.beans < latte.beans) {
+        terminateAction = true;
+        console.log("Sorry, not enough coffee beans!\n");
+    }
+    else if (coffeeMachine.cup < latte.cup) {
+        terminateAction =true;
+        console.log("Sorry, not enough cups!\n");
+    }
+    else {
+        buyLatte();
+        enoughResourcesMessage();
+    }
+}
+
+function checkResourcesForCappuccino() {
+    if (coffeeMachine.water < cappuccino.water) {
+        terminateAction = true;
+        console.log("Sorry, not enough water!\n");
+    }
+    else if (coffeeMachine.milk < cappuccino.milk) {
+        terminateAction = true;
+        console.log("Sorry, not enough milk!\n");
+    }
+    else if (coffeeMachine.beans < cappuccino.beans) {
+        terminateAction = true;
+        console.log("Sorry, not enough coffee beans!\n");
+    }
+    else if (coffeeMachine.cup < cappuccino.cup) {
+        terminateAction =true;
+        console.log("Sorry, not enough cups!\n");
+    }
+    else {
+        buyCappuccino();
+        enoughResourcesMessage();
+    }
+}
+
+function enoughResourcesMessage() {
+    console.log("I have enough resources, making you a coffee!\n");
+}
+
 function buyEspresso() {
     coffeeMachine.water = coffeeMachine.water - espresso.water;
     coffeeMachine.milk = coffeeMachine.milk - espresso.milk;
@@ -66,50 +143,51 @@ function getStateOfCoffeeMachine() {
     console.log(`${coffeeMachine.milk} ml of milk`);
     console.log(`${coffeeMachine.beans} g of coffee beans`);
     console.log(`${coffeeMachine.cup} disposable cups`);
-    console.log(`${coffeeMachine.money} of money\n`);
+    console.log(`$${coffeeMachine.money} of money\n`);
 }
 
 function start() {
-    getStateOfCoffeeMachine();
+    while(!terminateProgram) {
+        let action = input("Write action (buy, fill, take, remaining, exit):\n");
 
-    let action = input("Write action (buy, fill, take):\n");
+        switch (action) {
+            case buy:
+                let buyCoffee = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, " +
+                        "back - to main menu :\n");
 
-    switch (action) {
-        case buy:
-            buy = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n");
-            switch (buy) {
-                case "1":
-                    buyEspresso();
-                    getStateOfCoffeeMachine();
+                switch (buyCoffee) {
+                    case "1":
+                        checkResourcesForEspresso();
+                        break;
+                    case "2":
+                        checkResourcesForLatte();
+                        break;
+                    case "3":
+                        checkResourcesForCappuccino();
+                        break;
+                    }
                     break;
-                case "2":
-                    buyLatte();
-                    getStateOfCoffeeMachine();
-                    break;
-                case "3":
-                    buyCappuccino();
-                    getStateOfCoffeeMachine();
-                    break;
-            }
-            break;
-        case fill:
-            coffeeMachine.water = coffeeMachine.water +
-                Number(input("Write how many ml of water you want to add:\n"));
-            coffeeMachine.milk = coffeeMachine.milk +
-                Number(input("Write how many ml of milk you want to add:\n"));
-            coffeeMachine.beans = coffeeMachine.beans +
-                Number(input("Write how many grams of coffee beans you want to add:\n"));
-            coffeeMachine.cup = coffeeMachine.cup +
-                Number(input("Write how many coffee cups you want to add:\n"));
-
-            getStateOfCoffeeMachine();
-            break;
-        case take:
-            console.log(`I gave you ${coffeeMachine.money}$\n`);
-            coffeeMachine.money = 0;
-
-            getStateOfCoffeeMachine();
-            break;
+            case fill:
+                coffeeMachine.water = coffeeMachine.water +
+                    Number(input("Write how many ml of water you want to add:\n"));
+                coffeeMachine.milk = coffeeMachine.milk +
+                    Number(input("Write how many ml of milk you want to add:\n"));
+                coffeeMachine.beans = coffeeMachine.beans +
+                    Number(input("Write how many grams of coffee beans you want to add:\n"));
+                coffeeMachine.cup = coffeeMachine.cup +
+                    Number(input("Write how many coffee cups you want to add:\n"));
+                break;
+            case take:
+                console.log(`I gave you ${coffeeMachine.money}$\n`);
+                coffeeMachine.money = 0;
+                break;
+            case remaining:
+                getStateOfCoffeeMachine();
+                break;
+            case exit:
+                terminateProgram = true;
+                break;
+        }
     }
 }
 
